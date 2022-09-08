8d9b3feb5b69710f6d169efeed5667a393b782b7
chuarib <chuarib@gmail.com>
2022-09-08 09:10:12 +0800

    Final commit before deployment/ fixing google login issue

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,27 +1,28 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { HiMenu } from 'react-icons/hi';
-import { AiFillCloseCircle } from 'react-icons/ai';
-import { Link, Route, Routes } from 'react-router-dom';
-
-import { Sidebar, UserProfile } from '../components';
-import { userQuery } from '../utils/data';
-import { client } from '../client';
-import Pins from './Pins';
-import logo from '../assets/logo.png';
+import React, { useState, useRef, useEffect } from 'react'
+import { HiMenu } from 'react-icons/hi'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Sidebar, UserProfile } from '../components'
+import { client } from '../client'
+import logo from '../assets/logo.png'
+import Pins from './Pins'
+import { userQuery } from '../utils/data'
 
 const Home = () => {
-  const [toggleSidebar, setToggleSidebar] = useState(false);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null)
+  const [toggleSidebar, setToggleSidebar] = useState(false)
+  
   const scrollRef = useRef(null);
 
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
 
   useEffect(() => {
-    const query = userQuery(userInfo?.googleId);
+    const query = userQuery(userInfo?.sub);
 
     client.fetch(query).then((data) => {
       setUser(data[0]);
-    });
+    })
+    //eslint-disable-next-line
   }, []);
 
   useEffect(() => {
@@ -40,16 +41,16 @@ const Home = () => {
             <img src={logo} alt="logo" className="w-28" />
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
+            <img src={user?.image} alt={user?.name} className="w-9 h-9 rounded-full " />
           </Link>
         </div>
         {toggleSidebar && (
-        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-          <div className="absolute w-full flex justify-end items-center p-2">
-            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+            </div>
+            <Sidebar closeToggle={setToggleSidebar} user={user && user} />
           </div>
-          <Sidebar closeToggle={setToggleSidebar} user={user && user} />
-        </div>
         )}
       </div>
       <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
@@ -59,7 +60,7 @@ const Home = () => {
         </Routes>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
